575ea54c99016905d61ab4f6507bb11cd2e02dec
suleymansurucu <suleymansurucu95@gmail.com>
2025-01-07 13:32:18 -0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() {
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -31,7 +41,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Firebase cloud firestore Page'),
     );
   }
 }
@@ -55,27 +65,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  StreamSubscription? _streamSubscription;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // IDS
+    print(firestore.collection('users').id);
+    print(firestore.collection('users').doc().id);
+
     return Scaffold(
       appBar: AppBar(
         // TRY THIS: Try changing the color here to a specific color (to
@@ -87,39 +85,164 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  addToNewData();
+                },
+                child: Text('add To Data'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                )),
+            ElevatedButton(
+                onPressed: () {
+                  addToNewDataWithSet();
+                },
+                child: Text('Add to Data with Set'),
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.redAccent)),
+            ElevatedButton(
+                onPressed: () {
+                  updateToNewDataWithSet();
+                },
+                child: Text('Update to Data'),
+                style:
+                    ElevatedButton.styleFrom(backgroundColor: Colors.yellow)),
+            ElevatedButton(
+                onPressed: () {
+                  deleteToNewDataWithSet();
+                },
+                child: Text('Delete to Data'),
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.grey)),
+            ElevatedButton(
+                onPressed: () {
+                  readToNewDataWithSet();
+                },
+                child: Text('Read to Data one time'),
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.grey)),
+            ElevatedButton(
+                onPressed: () {
+                  readRalTimeToNewDataWithSet();
+                },
+                child: Text('Read to Data RealTime'),
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.blue)),
+            ElevatedButton(
+                onPressed: () {
+                  stopStream();
+                },
+                child: Text('Stop Stream'),
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueAccent)),
+            ElevatedButton(
+                onPressed: () {
+                  batchWord();
+                },
+                child: Text('Batch Word'),
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueAccent)),
+            ElevatedButton(
+                onPressed: () {
+                  transactionWord();
+                },
+                child: Text('Transaction Word'),
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueAccent)),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void addToNewData() async {
+    Map<String, dynamic> _addUser = {};
+    _addUser['name'] = 'eda';
+    _addUser['age'] = '24';
+    _addUser['isStudent'] = 'false';
+    _addUser['address'] = {
+      'street': '943 59th Street',
+      'streetLine2': '',
+      'city': 'Brooklyn',
+      'state': 'NY',
+      'Zip': '11222'
+    };
+    _addUser['colors'] = FieldValue.arrayUnion(['blue', 'green']);
+    _addUser['createAt'] = FieldValue.serverTimestamp();
+    await firestore.collection('users').add(_addUser);
+  }
+
+  Future<void> addToNewDataWithSet() async {
+    var _newDocID = firestore.collection('users').doc().id;
+    await firestore
+        .doc('users/$_newDocID')
+        .set({'name': 'Fatma Sara', 'userID': _newDocID});
+
+    await firestore.doc('users/Y507Xq9N7GCUI6ntZnMX').set(
+        {'school': 'brooklyn college', 'age': FieldValue.increment(1)},
+        SetOptions(merge: true));
+  }
+
+  Future<void> updateToNewDataWithSet() async {
+    await firestore
+        .doc('users/Y507Xq9N7GCUI6ntZnMX')
+        .update({'name': 'guncel Eda', 'isStudent': 'true'});
+  }
+
+  Future<void> deleteToNewDataWithSet() async {
+    await firestore.doc('users/Y507Xq9N7GCUI6ntZnMX').delete();
+  }
+
+  Future<void> readToNewDataWithSet() async {
+    var _usersDocuments = await firestore.collection('users').get();
+    print(_usersDocuments.size.toString());
+    print(_usersDocuments.docs.length.toString());
+    for (var eleman in _usersDocuments.docs) {
+      print('Document ID: ${eleman.id}');
+      Map userMap = eleman.data();
+      print(userMap['name']);
+    }
+    var _suleymanDoc = await firestore.doc('users/Dh0XYbXnj5UMgqpa08ja').get();
+    print(_suleymanDoc.data()!['address']['street']);
+  }
+
+  Future<void> readRalTimeToNewDataWithSet() async {
+    var userStream = await firestore.collection('users').snapshots();
+    _streamSubscription = userStream.listen((event) {
+      event.docChanges.forEach((element) {
+        print(element.doc.data().toString());
+      });
+    });
+  }
+
+  Future<void> stopStream() async {
+    await _streamSubscription?.cancel();
+  }
+
+  void batchWord() async{
+    WriteBatch _batch= firestore.batch();
+    CollectionReference _counterColRef = firestore.collection('counter');
+
+    // Create 100 items with together. If can wrong create in 100 items, for example 51 th item will be stop and then all items is not creating database
+    /*for(int  number=0; number<100;number++){
+      var _newDoc=_counterColRef.doc();
+      _batch.set(_newDoc, {'counter':++number, 'id' : _newDoc.id});
+    }*/
+    
+    /*var _counterDocs = await _counterColRef.get();
+    _counterDocs.docs.forEach((element){
+      _batch.update(element.reference, {'creatAt': FieldValue.serverTimestamp()});
+    });*/
+
+    var _counterDocs = await _counterColRef.get();
+    _counterDocs.docs.forEach((element){
+      _batch.delete(element.reference);
+    });
+
+    await _batch.commit();
+  }
+
+  void transactionWord() {
+
+
   }
 }
